53f2f8db03a7350a6bf51e65f33e80fdbc781482
monologg <adieujw@gmail.com>
2020-04-16 23:28:38 +0900

    Baseline for TPU

--- trainer.py
+++ trainer.py
@@ -10,6 +10,14 @@ from transformers import AdamW, get_linear_schedule_with_warmup
 
 from utils import set_seed, compute_metrics, get_labels, get_test_texts, show_report, MODEL_CLASSES
 
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torch_xla.debug.metrics as met
+import torch_xla.distributed.parallel_loader as pl
+import torch_xla.distributed.xla_multiprocessing as xmp
+import torch_xla.utils.utils as xu
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -35,7 +43,8 @@ class Trainer(object):
         self.model = self.model_class.from_pretrained(args.model_name_or_path, config=self.config)
 
         # GPU or CPU
-        self.device = "cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu"
+        # self.device = "cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu"
+        self.device = xm.xla_device()
         self.model.to(self.device)
 
         self.test_texts = None
@@ -104,7 +113,8 @@ class Trainer(object):
                 if (step + 1) % self.args.gradient_accumulation_steps == 0:
                     torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.args.max_grad_norm)
 
-                    optimizer.step()
+                    # optimizer.step()
+                    xm.optimizer_step(optimizer, barrier=True)
                     scheduler.step()  # Update learning rate schedule
                     self.model.zero_grad()
                     global_step += 1
